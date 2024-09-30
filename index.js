f05b33c13cb5ec1a61d88dc94d0b8467af960cc3
danieleamorimr <danieleamorim10@gmail.com>
2024-09-30 13:30:12 -0300

    projeto completo

---
+++ index.js
@@ -0,0 +1,39 @@
+let nomeHeroi = "Enzo";
+let experienciaXP = 1;
+
+
+if (experienciaXP <= 1000) {
+    console.log("O herói de nome:" + nomeHeroi + " , está no nivel: Ferro");
+
+} else if (experienciaXP >= 1001 && experienciaXP <= 2000 ) {
+    console.log("O herói de nome:" + nomeHeroi + " , está no nivel: Bronze");
+    
+} else if (experienciaXP >= 2001 && experienciaXP <= 5000) {
+console.log("O herói de nome:" + nomeHeroi + " , está no nivel: Prata");
+
+} else if (experienciaXP >= 5001 && experienciaXP <= 7000) {
+console.log("O herói de nome: " + nomeHeroi + " , está no nivel: Ouro");
+
+} else if (experienciaXP >= 7001 && experienciaXP <= 8000) {
+    console.log("O herói de nome:" + nomeHeroi + " , está no nivel: Platina");
+
+} else if (experienciaXP >= 8001 && experienciaXP <= 9000) {
+    console.log("O herói de nome:" + nomeHeroi + " , está no nivel: Ascendente");
+    
+} else if (experienciaXP >= 9001 && experienciaXP <= 10000) {
+    console.log("O herói de nome:" + nomeHeroi + " , está no nivel: Imortal");
+
+} else if (experienciaXP >= 10001) {
+    console.log("O herói de nome:" + nomeHeroi + " , está no nivel: Radiante");
+
+}else {
+    console.log("Você digitou uma opção não válida");
+}
+
+
+
+
+
+
+
+
